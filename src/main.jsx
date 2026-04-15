7e36ed0c9fc67c5d4d9a4a48ed7ed38a3182a2b3
Joe Testa <jtesta94@yahoo.com>
2026-04-14 23:41:59 -0400

    added vercel data metrics tracking

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Router } from "./router.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Analytics />
     <Router />
   </React.StrictMode>
 );
